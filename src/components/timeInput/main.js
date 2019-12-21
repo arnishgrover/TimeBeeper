ff21781426aaeb89724872f40e9f9c83a77149b3
Arnish Grover <arnishgrover@gmail.com>
2019-12-22 02:09:25 +0530

    Made the UI left, logic left

--- src/components/timeInput/main.js
+++ src/components/timeInput/main.js
@@ -1,25 +1,58 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Alert, ScrollView} from 'react-native';
 import {styles} from './styles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {OutlinedTextField} from 'react-native-material-textfield';
+import {Button} from 'react-native-elements';
 
 class timeInput extends Component {
-    render() {
-        return (
-            <View style={styles.main}>
-                <View style={[styles.first]}>
-                    <Text>The main screen</Text>
-                </View>
-                <View style={[styles.second]}>
-                    <Text>This would be the first piece</Text>
-                </View>
-                <View style={[styles.third]}>
-                    <Text>This would be the second piece</Text>
-                </View>
-                <View style={[styles.fourth]}>
-                    <Text>This would be the final piece</Text>
-                </View>
+    formatText = (text) => {
+        return text.replace(/[^+\d]/g, '');
+    };
 
-            </View>
+    state = {
+        color: 'white',
+    };
+
+    startTimer = () => {
+        this.setState({
+            color: 'green',
+        });
+        console.log('Starting the timer');
+    };
+
+    render() {
+
+        return (
+            <ScrollView contentContainerStyle={{flex: 1}}>
+                <View style={styles.main}>
+                    <View style={styles.first}>
+                        <View style={[styles.firstOne]}>
+                            <Text style={styles.topText}>Welcome to the Interval Beeper</Text>
+                        </View>
+                        <View style={[styles.firstSecond]}>
+                            <Icon.Button name="info-circle" color={'black'} iconStyle={styles.infoButton}
+                                         backgroundColor="white" onPress={() => {
+                                Alert.alert('Button clicked');
+                            }}/>
+                        </View>
+                    </View>
+                    <View style={[styles.second]}>
+                        <Text style={styles.secondText}>Enter the time interval for the beeping</Text>
+                    </View>
+                    <View style={[styles.third]}>
+                        <OutlinedTextField label='Time' keyboardType='phone-pad' fontSize={32}
+                                           formatText={this.formatText} containerStyle={styles.timeInput}/>
+                        <Text style={[styles.timerStarted, {color: this.state.color}]}>Beeper started</Text>
+                        <Icon name={'minus-circle'} size={30} style={styles.beeperStartIcon} color={this.state.color}/>
+                    </View>
+                    <View style={[styles.fourth]}>
+                        <Button icon={{name: 'check', size: 24, color: 'white'}} title="START" type="solid"
+                                raised={true} onPress={this.startTimer} containerStyle={styles.startButtonContainer}
+                                buttonStyle={styles.startButton}/>
+                    </View>
+                </View>
+            </ScrollView>
         );
     }
 }
--- src/components/timeInput/styles.js
+++ src/components/timeInput/styles.js
@@ -2,28 +2,72 @@ import {StyleSheet} from 'react-native';
 export const styles = StyleSheet.create({
     main: {
         flex: 1,
-        borderColor: "black",
+        borderColor: "white",
         borderWidth: 1,
         margin: 5
     },
     first: {
         flex: 1,
-        borderColor: "blue",
-        borderWidth: 1
+        borderColor: "white",
+        borderWidth: 1,
+        flexDirection: 'row',
+        paddingTop: 10,
+        paddingLeft: 15
     },
     second: {
         flex: 1,
-        borderColor: "green",
+        borderColor: "white",
         borderWidth: 1
     },
     third: {
-        flex: 3,
-        borderColor: "red",
+        flex: 2,
+        borderColor: "white",
         borderWidth: 1
     },
     fourth: {
-        flex: 2,
-        borderColor: "yellow",
+        borderColor: "white",
         borderWidth: 1
+    },
+    topText: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif-light',
+        color: "rgba(12,12,12,0.81)",
+        textAlign: 'center',
+        paddingTop: 20
+    },
+    infoButton: {
+        paddingTop: 25,
+        paddingLeft: 15
+    },
+    secondText: {
+        fontSize: 30,
+        textAlign: "center",
+        paddingTop: 20,
+        fontFamily: 'sans-serif-thin'
+    },
+    timeInput: {
+        alignContent: 'center',
+        padding: 15,
+        paddingTop: 80,
+    },
+    startButtonContainer: {
+        margin: 10
+    },
+    startButton: {
+        padding: 20
+    },
+    firstOne: {
+        flex: 4
+    },
+    firstSecond: {
+        flex: 1
+    },
+    timerStarted: {
+        textAlign: 'center',
+        fontSize: 24
+    },
+    beeperStartIcon : {
+        alignSelf: 'center'
     }
 });
